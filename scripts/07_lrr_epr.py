54d0734351d1d5f352d90fc60272a0c4d3596069
adriahernandezqueralt <adria.hernandez.queralt@estudiantat.upc.edu>
2026-06-23 10:32:37 +0200

    Create 07_lrr_epr.py

---
+++ scripts/07_lrr_epr.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+archivo = r"C:\Users\FX506\Desktop\tfg python\Grafiques_tfg.xlsx"
+
+df = pd.read_excel(archivo, sheet_name="B2", header=3)
+df.columns = df.columns.str.strip()
+df = df.sort_values("TransOrder")
+
+plt.figure(figsize=(8, 5))
+plt.plot(df["TransOrder"], df["LRR"], marker="o")
+plt.axhline(0)
+
+plt.title("LRR Rates Baseline B")
+plt.xlabel("Transecte")
+plt.ylabel("LRR")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("LRR_transectes.png", dpi=300)
+plt.show()
+
+plt.figure(figsize=(8, 5))
+plt.plot(df["TransOrder"], df["EPR"], marker="o")
+plt.axhline(0)
+
+plt.title("EPR Rates Baseline B")
+plt.xlabel("Transecte")
+plt.ylabel("EPR")
+plt.grid(True)
+plt.tight_layout()
+plt.savefig("EPR_transectes.png", dpi=300)
+plt.show()
+
