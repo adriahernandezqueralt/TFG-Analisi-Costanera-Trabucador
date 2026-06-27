b36f62939ba76ff4af067b0fef05a507b87752c9
adriahernandezqueralt <adria.hernandez.queralt@estudiantat.upc.edu>
2026-06-27 18:58:28 +0200

    Update 07_lrr_epr.py

--- scripts/07_lrr_epr.py
+++ scripts/07_lrr_epr.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-archivo = r"C:\Users\FX506\Desktop\tfg python\Grafiques_tfg.xlsx"
+archivo = "Tu URL"
 
 df = pd.read_excel(archivo, sheet_name="B2", header=3)
 df.columns = df.columns.str.strip()
